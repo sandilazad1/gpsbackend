b945b59661c128dbb9bf254ef3b2f5f82b2e0f69
Sandil Azad <sandilazad@gmail.com>
2022-04-17 23:34:08 +0600

    bodyparsher

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
 const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded());
 
 const PORT = process.env.PORT || 3000
+
+app.post('api/obdi/data',(req,res)=>{
+    const odbiData = req.body
+    console.log(odbiData)
+});
 
 app.listen(PORT,()=>{
     console.log(`server in running ${PORT}`);
